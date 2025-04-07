5121c9e8831c15125a1a8d894775503840805ab5
AadarshHegde2025 <aadarsh.hegde@gmail.com>
2025-04-07 16:02:40 -0500

    seeing if it was a queue bug

--- server/server.go
+++ server/server.go
@@ -79,7 +79,7 @@ func sendAutoscalerStatistics() { // only send when a job with 'key' has complet
 	}
 
 	mu.Lock()
-	server_stats := rpcstructs.ServerUsage{my_ip, compute_remaining, memory_remaining, job_to_timing, job_queue_len, status, time.Now().Unix(), my_type}
+	server_stats := rpcstructs.ServerUsage{my_ip, compute_remaining, memory_remaining, job_to_timing, len(job_queue), status, time.Now().Unix(), my_type}
 	var reply string
 	err = autoscaler.Call("AutoScaler.RequestedStats", &server_stats, &reply)
 	if err != nil {
