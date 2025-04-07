05f9361b79d4a596e846d92973d69dc4d8a504f1
AadarshHegde2025 <aadarsh.hegde@gmail.com>
2025-04-06 20:52:37 -0500

    all merging complete

--- autoscaler.go
+++ autoscaler.go
@@ -11,7 +11,6 @@ import (
 	"os"
 	"os/exec"
 	"os/signal"
-	"strconv"
 	"strings"
 	"sync"
 	"syscall"
@@ -32,11 +31,66 @@ type AutoScaler struct{}
 
 var server_to_status_overtime = make(map[string][]rpcstructs.ServerUsage)
 
-var server_to_status = make(map[string]rpcstructs.ServerUsage) // server_ip -> ServerStatus
-var job_completion_times = make(map[string][]int64)            // how much time between when the job was added to the server and when it was completed, for each server
-var job_execution_times = make(map[string][]int64)             // how much time the job took to execute, for each server
+var server_to_status = make(map[string]ServerState) // server_ip -> ServerStatus
+var job_completion_times = make(map[string][]int64) // how much time between when the job was added to the server and when it was completed, for each server
+var job_execution_times = make(map[string][]int64)  // how much time the job took to execute, for each server
 
 var mu sync.Mutex
+
+var snapshotMutex sync.Mutex
+
+type Status int
+
+type ServerState struct {
+	Status Status // True= Online, False = Offline
+
+	Server_Type ServerType // Compute or memory heavy
+
+	ComputeRemaining float64 //
+
+	MemoryRemaining float64 //
+
+}
+
+// Define the enum values as constants
+
+const (
+	ONLINE Status = iota
+
+	OFFLINE
+)
+
+type ServerType int
+
+const (
+	COMPUTE_HEAVY ServerType = iota
+
+	MEMORY_HEAVY
+)
+
+type SnapshotListNode struct {
+	next *SnapshotListNode
+
+	prev *SnapshotListNode
+
+	data rpcstructs.Snapshot
+}
+
+type SnapshotList struct {
+	head *SnapshotListNode
+
+	tail *SnapshotListNode
+}
+
+var snapshotList SnapshotList = SnapshotList{}
+
+// Here, x denotes the number of compute heavy VMs that are currently online
+
+var x int = 0
+
+// Here, y denotes the number of compute heavy VMs that are currently online
+
+var y int = 0
 
 func plotOverlayedMetric(title, filename, ylabel string, allData map[string]plotter.XYs) {
 	p := plot.New()
@@ -158,20 +212,14 @@ func (t *AutoScaler) RequestedStats(args *rpcstructs.ServerUsage, reply *string)
 	mu.Lock()
 	fmt.Println("Received server stats:", args.ServerIp, args.ComputeRemaining, args.MemoryRemaining)
 	status := server_to_status[args.ServerIp] // mark the server as online
-	status.Status = args.Status
-	if status.Status == false {
+	status.Status = ONLINE
+	if status.Status == OFFLINE {
 		fmt.Println("Server , ", args.ServerIp, " is now offline")
 	}
 	status.ComputeRemaining = args.ComputeRemaining
 	status.MemoryRemaining = args.MemoryRemaining
-	status.JobToTiming = args.JobToTiming
-	status.QueueLength = args.QueueLength
-	status.Time = args.Time
 	server_to_status[args.ServerIp] = status
 
-	status2 := server_to_status_overtime[args.ServerIp]
-	status2 = append(status2, status)
-	server_to_status_overtime[args.ServerIp] = status2
 	// TODO : Add logic to store the stats in some data structure so that we can do predictive autoscaling
 
 	mu.Unlock()
@@ -189,7 +237,323 @@ func adjust_server(power_flag bool) { // if true turn on, if false turn off, nee
 	}
 }
 
-func autoscale() {
+func (t *AutoScaler) AddSnapshotToList(args *rpcstructs.Snapshot, reply *string) error {
+
+	snapshotMutex.Lock()
+
+	defer snapshotMutex.Unlock()
+
+	snapshot := rpcstructs.Snapshot{JobType: args.JobType, CpuUtilization: args.CpuUtilization, MemoryUtilization: args.MemoryUtilization}
+
+	// There is no node in our list, add the new node as the head and tail
+
+	if snapshotList.head == nil {
+
+		snapshotNode := &SnapshotListNode{nil, nil, snapshot}
+
+		snapshotList.head = snapshotNode
+
+		snapshotList.tail = snapshotNode
+
+		// There is a node in our list already, add the new node as the tail
+
+	} else {
+
+		snapshotNode := &SnapshotListNode{nil, snapshotList.tail, snapshot}
+
+		snapshotList.tail.next = snapshotNode
+
+		snapshotList.tail = snapshotNode
+
+	}
+
+	*reply = "Snapshot Added"
+
+	return nil
+
+}
+
+func truncateHistory(list *SnapshotList, node *SnapshotListNode) {
+
+	node.prev = nil
+
+	list.head = node
+
+}
+
+func findOptimalConfiguration(num_compute_heavy_available int, num_memory_heavy_available int) (int, int) {
+
+	var x_prime int
+
+	var y_prime int
+
+	var maxVal float64 = math.Inf(-1)
+
+	for i := 0; i < num_compute_heavy_available; i++ {
+
+		for j := 0; j < num_memory_heavy_available; j++ {
+
+			utility := calculateUtilityFunction(i, j)
+
+			if utility > maxVal {
+
+				maxVal = utility
+
+				x_prime = i
+
+				y_prime = j
+
+				// In case of tiebreak choose configuration that requires least change of servers
+
+			} else if utility == maxVal &&
+
+				(math.Abs(float64(x_prime-x))+math.Abs(float64(y_prime-y)) > math.Abs(float64(i-x))+math.Abs(float64(j-y))) {
+
+				maxVal = utility
+
+				x_prime = i
+
+				y_prime = j
+
+			}
+
+		}
+
+	}
+
+	return x_prime, y_prime
+
+}
+
+func calculateUtilityFunction(x_prime int, y_prime int) float64 {
+
+	var alpha float64 = 1.0
+
+	var lambda float64 = 1.0
+
+	var gamma float64 = 1.0
+
+	// First term
+
+	var A float64 = 0
+
+	var B float64 = 0
+
+	var C float64 = 0
+
+	var D float64 = 0
+
+	A, B = calculateExpectedLatencyAndUtilization(x_prime, y_prime)
+
+	C = float64(x_prime + y_prime)
+
+	D = float64(max(x_prime-x, 0) + max(y_prime-y, 0))
+
+	return alpha*A + lambda*B + C + gamma*D
+
+}
+
+func calculateExpectedLatencyAndUtilization(x_prime int, y_prime int) (float64, float64) {
+
+	snapshotMutex.Lock()
+
+	defer snapshotMutex.Unlock()
+
+	const N = 10
+
+	// Look through linked list backwards until we find a timestamp that was N seconds before now
+
+	now := time.Now().Unix()
+
+	current := snapshotList.tail
+
+	for {
+
+		if current == nil {
+			break
+		}
+
+		if current.data.Timestamp < now-N {
+
+			// Remove all data from the linked list that is before our threshold (N)
+
+			truncateHistory(&snapshotList, current.next)
+
+			break
+
+		}
+
+		current = current.prev
+
+	}
+
+	// TODO: Implement simulation of queues logic
+
+	// Initialize queues
+
+	compute_queues := make([][]rpcstructs.Snapshot, x_prime)
+
+	for i := range compute_queues {
+
+		compute_queues[i] = make([]rpcstructs.Snapshot, 0)
+
+	}
+
+	memory_queues := make([][]rpcstructs.Snapshot, y_prime)
+
+	for i := range memory_queues {
+
+		memory_queues[i] = make([]rpcstructs.Snapshot, 0)
+
+	}
+
+	// Assign jobs from the linked list to the queue
+
+	current = snapshotList.head
+
+	compute_queue_index := 0
+
+	memory_queue_index := 0
+
+	for {
+
+		if current == nil {
+
+			break
+
+		}
+
+		// Add job to a queue depending on what type it is
+
+		jobType := current.data.JobType
+
+		switch jobType {
+
+		case rpcstructs.COMPUTE_HEAVY:
+
+			compute_queues[compute_queue_index] = append(compute_queues[compute_queue_index], current.data)
+
+			compute_queue_index = (compute_queue_index + 1) % len(compute_queues)
+
+		case rpcstructs.MEMORY_HEAVY:
+
+			memory_queues[memory_queue_index] = append(memory_queues[memory_queue_index], current.data)
+
+			memory_queue_index = (memory_queue_index + 1) % len(memory_queues)
+
+		}
+
+		current = current.next
+
+	}
+
+	// for each compute heavy queue
+
+	// t
+
+	var total_latency int64 = 0
+
+	total_jobs := 0
+
+	ComputeRunDuration := 0
+
+	var total_cpu_utilization float64 = 0
+
+	for q_idx := range compute_queues {
+
+		// figure out utilization over the course of the dataset (both memory and cpu)
+
+		// If the compute_queue is empty, do no calculation
+
+		if len(compute_queues[q_idx]) == 0 {
+			continue
+		}
+
+		job_start_time := compute_queues[q_idx][0].Timestamp
+
+		for j_idx := range compute_queues[q_idx] {
+
+			job := compute_queues[q_idx][j_idx]
+
+			// When the job will finish (the same as when the next job is allowed to start)
+
+			job_true_start := max(job_start_time, job.Timestamp)
+
+			job_finish_time := job_true_start + job.ExecutionTime
+
+			// Latency = time when job finished - time when job came in
+
+			job_latency := job_finish_time - job.Timestamp
+
+			// Weight cpu utilization with time spent processing
+
+			total_cpu_utilization += job.CpuUtilization * float64(job.ExecutionTime)
+
+			job_start_time = job_finish_time
+
+			total_latency += job_latency
+
+		}
+
+		ComputeRunDuration += int(job_start_time)
+
+		total_jobs += len(compute_queues[q_idx])
+
+	}
+
+	var total_memory_utilization float64 = 0
+
+	MemoryRunDuration := 0
+
+	for q_idx := range memory_queues {
+
+		// figure out utilization over the course of the dataset (both memory and cpu)
+
+		// If the compute_queue is empty, do no calculation
+
+		if len(memory_queues[q_idx]) == 0 {
+			continue
+		}
+
+		job_start_time := memory_queues[q_idx][0].Timestamp
+
+		for j_idx := range memory_queues[q_idx] {
+
+			job := memory_queues[q_idx][j_idx]
+
+			// When the job will finish (the same as when the next job is allowed to start)
+
+			job_true_start := max(job_start_time, job.Timestamp)
+
+			job_finish_time := job_true_start + job.ExecutionTime
+
+			// Latency = time when job finished - time when job came in
+
+			job_latency := job_finish_time - job.Timestamp
+
+			// Weight memory utilization with
+
+			total_memory_utilization += job.MemoryUtilization * float64(job.ExecutionTime)
+
+			job_start_time = job_finish_time
+
+			total_latency += job_latency
+
+		}
+
+		MemoryRunDuration += int(job_start_time)
+
+		total_jobs += len(memory_queues[q_idx])
+
+	}
+
+	average_utilization := total_memory_utilization/float64(MemoryRunDuration) + total_cpu_utilization/float64(ComputeRunDuration)
+
+	return float64(total_latency) / float64(total_jobs), average_utilization
+
+}
+
+func autoscale(num_compute_heavy_available int, num_memory_heavy_available int) {
 	// TODO: Write actual algorithm for autoscaling here
 
 	// autoscaler has to be aware of which servers are online and offline so it knows what can be turned off or on
@@ -199,41 +563,42 @@ func autoscale() {
 	// autoscaler also has to let load balancer know when it adds or removes a server
 
 	// basic testing that autoscaler can interact with load balancer
-	time.Sleep(30 * time.Second) // wait for load balancer to start
-	fmt.Println("Autoscaler is starting to send stats to load balancer")
+	for {
 
-	load_balancer, err := rpc.Dial("tcp", LOAD_BALANCER_IP+":"+strconv.Itoa(port))
-	if err != nil {
-		fmt.Println("Error connecting to load balancer:", err)
-		return
-	}
+		x_prime, y_prime := findOptimalConfiguration(num_compute_heavy_available, num_memory_heavy_available)
 
-	var reply int
+		// The optimal configuration has changed scale up or down servers
 
-	// Add servers from sp25-cs525-0906 to sp25-cs525-0918 (inclusive)
-	for i := 6; i <= 9; i++ {
-		hostname := fmt.Sprintf("sp25-cs525-09%02d.cs.illinois.edu", i)
-		nodeNumber := i - 1 // Or however you want to map this
-		args := rpcstructs.ServerDetails{hostname, nodeNumber, "C"}
+		if x_prime != x || y_prime != y {
 
-		err := load_balancer.Call("ServerChange.AddServer", &args, &reply)
-		if err != nil {
-			fmt.Printf("RPC AddServer failed for %s: %v\n", hostname, err)
+			if x_prime > x {
+
+				// Add (x_prime - x) compute heavy server
+
+			}
+
+			if y_prime > y {
+
+				// Add (y_prime - y) memory heavy servers
+
+			}
+
+			if x_prime < x {
+
+				// Sort compute heavy servers by queue size, remove (x - x_prime) servers with the shortest queues
+
+			}
+
+			if y_prime < y {
+
+				// Sort memory heavy servers by queue size, remove (y - y_prime) servers with the shortest queues
+
+			}
+
 		}
-	}
+		// attempt autoscale every 5 seconds (?)
+		time.Sleep(5 * time.Second)
 
-	time.Sleep(20 * time.Second) // simulate some time passing before removals
-
-	// Remove the same servers
-	for i := 6; i <= 9; i++ {
-		hostname := fmt.Sprintf("sp25-cs525-09%02d.cs.illinois.edu", i)
-		nodeNumber := i - 1
-		args := rpcstructs.ServerDetails{hostname, nodeNumber, "C"}
-
-		err := load_balancer.Call("ServerChange.RemoveServer", &args, &reply)
-		if err != nil {
-			fmt.Printf("RPC RemoveServer failed for %s: %v\n", hostname, err)
-		}
 	}
 
 }
@@ -271,14 +636,49 @@ func main() {
 	num_initially_online := scanner.Text()
 	fmt.Println("Number of servers: ", num_servers)
 	fmt.Println("Number of initially online servers: ", num_initially_online)
+
+	var num_memory_heavy_available int = 0
+	var num_compute_heavy_available int = 0
 	for scanner.Scan() {
 		line = scanner.Text()
 		words := strings.Fields(line)
-		server_to_status[words[1]] = rpcstructs.ServerUsage{Status: false, Server_Type: words[4], ComputeRemaining: -1, MemoryRemaining: -1} // everything starts offline until they identify themselves, -1 for resource util until known
+		var server_type ServerType
+
+		switch words[3] {
+
+		case "C":
+
+			server_type = COMPUTE_HEAVY
+
+		case "M":
+
+			server_type = MEMORY_HEAVY
+
+		// TODO: Add more robust default here (e.g. unspecified) and error checking
+
+		default:
+
+			server_type = COMPUTE_HEAVY
+
+		}
+
+		server_to_status[words[1]] = ServerState{Status: OFFLINE, Server_Type: server_type, ComputeRemaining: -1, MemoryRemaining: -1} // everything starts offline until they identify themselves, -1 for resource util until known
+
+		// Count whether server is online or offline
+
+		if server_type == MEMORY_HEAVY {
+
+			num_memory_heavy_available += 1
+
+		} else if server_type == COMPUTE_HEAVY {
+
+			num_compute_heavy_available += 1
+
+		}
 	}
 
-	go startAutoscaler() // handler to receive stats from servers
-	go autoscale()       // actual autoscaling logic
+	go startAutoscaler()                                                  // handler to receive stats from servers
+	go autoscale(num_compute_heavy_available, num_memory_heavy_available) // actual autoscaling logic
 
 	sigChan := make(chan os.Signal, 1)
 	signal.Notify(sigChan, os.Interrupt, syscall.SIGTERM)
