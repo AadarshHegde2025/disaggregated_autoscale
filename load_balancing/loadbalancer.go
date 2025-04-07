e51065b3aea518f72164ab2d9d7b2114e079331c
AadarshHegde2025 <aadarsh.hegde@gmail.com>
2025-04-06 22:46:52 -0500

    testing

--- load_balancing/loadbalancer.go
+++ load_balancing/loadbalancer.go
@@ -129,7 +129,7 @@ func resource_awareness_loadbalancer() {
 		var client *rpc.Client
 		var server_ip string
 		fmt.Println((plan_cpu / (100 * 12)), plan_mem)
-		if (plan_cpu / (100 * 8)) > plan_mem {
+		if (plan_cpu / (100 * 16)) > plan_mem {
 			server_ip = compute_online_servers[compute_i%len(compute_online_servers)]
 
 			client, _ = rpc.Dial("tcp", server_ip+":"+strconv.Itoa(port))
