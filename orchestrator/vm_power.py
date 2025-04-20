a9b420aa7586071803e581e2224f333fbae5a1a7
aadarshhegde <ahegd2@illinois.edu>
2025-04-20 17:17:12 -0500

    another bug fix with command sending

--- orchestrator/vm_power.py
+++ orchestrator/vm_power.py
@@ -35,8 +35,9 @@ def ssh_and_run(vm_number, cpu, mem):
     shell = client.invoke_shell()
     commands = [
         "cd disaggregated_autoscale",
-        f"nohup GOTOOLCHAIN=auto go run server/server.go -cpu={cpu} -mem={mem} > server.log 2>&1 &"
+        f"nohup bash -c 'GOTOOLCHAIN=auto go run server/server.go -cpu={cpu} -mem={mem}' > server.log 2>&1 &"
     ]
+
 
     for cmd in commands:
         print(f"Running on VM{vm_number}: {cmd}")
